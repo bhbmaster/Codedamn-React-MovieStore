1a212bdd41c0c3900362378976fa11c7f3665fc3
bhbmaster <bhbmaster@gmail.com>
2020-11-16 03:58:48 -0800

    codedamn react finished video 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css'
 import { Button } from '@material-ui/core'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Button variant="contained" color="primary">Hello World</Button>
     </div>
   );
