2ee0897596a24e5e118d88a6893a4d9b87738133
bhbmaster <bhbmaster@gmail.com>
2020-11-22 07:15:05 -0800

    codedamn react finished video 27 - adding loader

--- src/components/Movies/Movie/index.tsx
+++ src/components/Movies/Movie/index.tsx
@@ -10,12 +10,12 @@ type Props = {
 //const Movie: React.FC = props => {
 const Movie: React.FC<Props> = props => {
 
-    console.log("movie props", props)
+    //console.log("movie props", props)
 
     return (
         <div className="movie">
             <h2>{props.title}</h2>
-            <img src={props.image}/>
+            <img alt={props.title + " Poster"}src={props.image}/>
             <h3>{props.year}</h3>
         </div>
     )
--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -1,71 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import Movie from './Movie'
 import './style.css'
+import { CircularProgress } from '@material-ui/core'
 
 const API_KEY = "eb7f19c3"  // codecamps - my key  383e0fe3
 
-//const series = [ 'the triangle', 'avengers', 'inseption', 'interstellar' ]
 const series = [ 'the triangle', 'avengers', 'inception', 'interstellar', 'harry potter', 'lord of the rings', 'the expanse' ]
-
-//const series = [ 'avengers', 'interstellar' ]
 
 const Movies: React.FC = props => {
 
     const [movies,setMovies] = useState([])
 
     useEffect(() => {
-        //console.log("Effect")
         const promises = series.map(series=>{return fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(series)}&apikey=${API_KEY}&page=1`).then(
             r=>{
                 let answer=r.json()
-                console.log("here:",answer);
+                //console.log("here:",answer);
                 return answer;
             }
         )
         })
 
-        //console.log("promises array:", promises)
-        //Promise.all(promises).then(r=>console.log("PA-Success",r)).catch(r=>console.log("PA-fail",r))
         Promise.all(promises).then((movies: any) => {
-            //setMovies(movies.map((m: any) => m.Search))
-            //let filteredmovies = movies.filter((s: any)=>s.imdbID!==undefined)
-            //console.log("FILTERED:",filteredmovies)
-            setMovies(movies)
-            //setMovies(filteredmovies)
-            
-
+           setMovies(movies)
+           //setMovies(movies.map((movie: any) => movie.Search))
          } )
     }, [])
-    
 
-    // useEffect(() => {
-    //     series.forEach(s=>{
-    //         return fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(s)}&apikey=${API_KEY}`).then(r=>r.json()).then(r=>console.log(r))
-    //     })
-    // }, [])
+    //console.log("MOVIES:", movies)
 
-    // useEffect(() => {
-    //     //console.log("Fetching1")
-    //     fetch(`http://www.omdbapi.com/?t=C&y=2019&apikey=${API_KEY}`).then(r=>r.json()).then(r=>console.log("Le Response:",r))
-    //     // fetch(`http://www.omdbapi.com/?t=C&y=2019&apikey=${API_KEY}`).then(r=>{ var ans=r.json(); console.log("Our Res", ans); return ans})
-    // }, [])
-
-    // return (
-    //     <div>
-    //         test
-    //     </div>
-    // )
-
-    // return <div className="movies">
-    //         { movies.flat(2).map((movie:any) => {
-    //             <Movie key={movie.imdbID}
-    //             title={movie.title}
-    //             year={movie.Year}
-    //         image={movie.Poster} /> })}
-    //     </div>
-
-    console.log("MOVIES:", movies)
-    // movies.forEach(m=>console.log("important stuff:", m.imdbID, m.Title, m.Year, m.Poster))
+    if (movies.length === 0) {
+        console.log("Loader")
+        return ( 
+                <div className="loader">
+                    <CircularProgress/>
+                </div>
+        )
+    }
 
     return <div className="movies">
             { movies.flat(2).map((movie:any) => 
