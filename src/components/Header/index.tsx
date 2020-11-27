f9b77137477aee4212ce21dafbbeea7f782f4c17
bhbmaster <bhbmaster@gmail.com>
2020-11-27 03:50:38 -0800

    removed useless icons from App Bar and edited readme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { AppBar, Badge, IconButton, InputBase, Toolbar, Typography } from '@material-ui/core'
-import { More as MoreIcon, Menu as MenuIcon, Search as SearchIcon, Mail as MailIcon, Notifications as NotificationsIcon, AccountCircle} from '@material-ui/icons'
+import { AppBar, InputBase, Toolbar, Typography } from '@material-ui/core'
+import { Search as SearchIcon} from '@material-ui/icons'
 import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -65,13 +65,6 @@ const Header: React.FC<Props> = props => {
   return (
     <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" noWrap>
             Our Movie Store
           </Typography>
@@ -87,36 +80,7 @@ const Header: React.FC<Props> = props => {
               inputProps={{ 'aria-label': 'search' }}
             />
           </div>
-          <div style={{ flexGrow: 1}}/>
-          <div >
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </div>
-          <div >
-            <IconButton
-              aria-label="show more"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
+
         </Toolbar>
       </AppBar>
 ) 
